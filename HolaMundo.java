9ec80ec835b1d9c0b9514b7e9ca20fe1727295b1
JavierMateoEdo <javimat.edo@gmail.com>
2019-12-04 10:55:20 +0100

    Otro HolaMundo

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,4 +1,4 @@
 public class HolaMundo{
-   public static void main(String[] args){ System.out.println("Hola mundo"); }
+   public static void main(String[] args){ System.out.println("Otra Hola mundo"); }
 }
 
